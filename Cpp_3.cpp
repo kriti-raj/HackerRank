0dd30b8f140d0b5b28ea91ccb07387779e1ab475
Kriti Raj <6517kritiraj@gmail.com>
2021-12-27 16:21:24 +0530

    Cpp added

---
+++ Cpp_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    // cin>>a;
+    // cin>>b;
+    // cin>>c;
+    // cin>>d;
+    // cin>>e;
+    // cout<<int(a)<<endl;
+    // cout<<long(b)<<endl;
+    // cout<<char(c)<<endl;
+    // cout<<float(d)<<endl;
+    // cout<<double(e)<<endl;
+    scanf("%d %ld %c %f %lf", &a, &b, &c ,&d, &e);
+    printf("%d\n%ld\n%c\n%f\n%lf", a, b, c ,d ,e);
+    return 0;
+}
